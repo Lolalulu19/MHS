623629aa4f19feda45a95c32ab05bc69b69f049f
HaniDayana <145449469+nhanidayana@users.noreply.github.com>
2023-10-27 23:56:05 +0800

    StackedBarchartAPI.py

--- StackedBarchartAPI.py
+++ StackedBarchartAPI.py
@@ -1,28 +1,37 @@
 import requests
+import pandas as pd
 import matplotlib.pyplot as plt
-
-# Retrieve data from the API
 
 # Define the header parameters
 headers = {
-        "Accept": "application/vnd.BNM.API.v1+json"
+    "Accept": "application/vnd.BNM.API.v1+json"
 }
 
-
 api_url = "https://api.bnm.gov.my/public/kijang-emas"
-
 response = requests.get(api_url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()["data"]
+
+    # Extracting data for DataFrame
     quantities = ['One Ounce', 'Half Ounce', 'Quarter Ounce']
     buying_prices = [data["one_oz"]["buying"], data["half_oz"]["buying"], data["quarter_oz"]["buying"]]
     selling_prices = [data["one_oz"]["selling"], data["half_oz"]["selling"], data["quarter_oz"]["selling"]]
 
+    # Creating DataFrame
+    df = pd.DataFrame({
+        'Quantities': quantities,
+        'Buying Price': buying_prices,
+        'Selling Price': selling_prices
+    })
+
+    # Print the DataFrame (optional)
+    print(df)
+
     # Create a stacked bar chart
     x = range(len(quantities))
-    plt.bar(x, buying_prices, label='Buying Price', color='b')
-    plt.bar(x, selling_prices, label='Selling Price', color='g', bottom=buying_prices)
+    plt.bar(x, df['Buying Price'], label='Buying Price', color='b')
+    plt.bar(x, df['Selling Price'], label='Selling Price', color='g', bottom=df['Buying Price'])
     plt.xlabel('Quantities')
     plt.ylabel('Prices')
     plt.title('Stacked Bar Chart: Gold Prices for Different Quantities')
@@ -33,8 +42,3 @@ if response.status_code == 200:
 
 else:
     print("Failed to retrieve data from the API.")
-
-
-
-
-
